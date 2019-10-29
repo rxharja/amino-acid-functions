4f91e7f27a03ad4efa530c0f6a32c248c2999eaf
B151359-2019 <email_not_found>
2019-10-29 12:13:29 +0000

    added comments to amino_acids.py

--- amino_acids.py
+++ amino_acids.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+#given a sequence and amino acid residue, returns the percent that residue encompasses
 def amino_acid_percentages_v1(sequence,residue):
 	try:
 		return ( sequence.upper().count(residue.upper())/len(sequence) ) * 100
@@ -10,7 +10,7 @@ assert round(amino_acid_percentages_v1("MSRSLLLRFLLFLLLLPPLP", "M")) == round(5)
 assert round(amino_acid_percentages_v1("MSRSLLLRFLLFLLLLPPLP", "r")) == round(10)
 assert round(amino_acid_percentages_v1("MSRSLLLRFLLFLLLLPPLP", "L")) == round(50)
 assert round(amino_acid_percentages_v1("MSRSLLLRFLLFLLLLPPLP", "Y")) == round(0)
-
+#given a sequence and a list of residues, will return the total percentage of all given residues
 def amino_acid_percentages_v2(sequence, residue=['A','I','L','M','F','W','Y','V']):
 	try:
 		total = 0
@@ -22,7 +22,7 @@ def amino_acid_percentages_v2(sequence, residue=['A','I','L','M','F','W','Y','V'
 assert round(amino_acid_percentages_v2("MSRSLLLRFLLFLLLLPPLP", ["M"])) == 5
 assert round(amino_acid_percentages_v2("MSRSLLLRFLLFLLLLPPLP", ['F', 'S', 'L'])) == 70
 assert round(amino_acid_percentages_v2("MSRSLLLRFLLFLLLLPPLP")) == 65
-
+#given a dna sequence and a threshold, returns a bool if the count is greater than the threshold
 def base_counter(seq, threshold):
 	try:
 		count = (sum(1 for nuc in seq.upper() if nuc not in "ATCG")/len(seq))*100
@@ -34,7 +34,7 @@ assert base_counter("ATCGnnnn",50) == True
 assert base_counter("ATCGnnnn",51) == False
 assert base_counter("ATGCGTAtttTTGAGCAnnnnnnnnnnn",35) == True
 assert base_counter("ATGCGTAtttTTGAGCAnnnnnnnnnnn",50) == False
-
+#given a dna sequence, a kmer size, and a minimum frequency, prints the kmers that occur more often than the minimum frequency
 def kmer_counting(dna,kmersize,minfrequency):
 	kmers = []
 	[kmers.append(dna[idx:idx+kmersize]) for idx in range(len(dna)) if len(dna[idx:idx+kmersize]) == kmersize]	
@@ -43,6 +43,7 @@ def kmer_counting(dna,kmersize,minfrequency):
 
 #kmer_counting("ATGCATCATG",2,2)	
 
+#takes arguments from kmer_counting and turns them into user inputs and places them in try/catch block
 def kmer_user_input():
 	try:
 		dna = input("Please enter your sequence:")
