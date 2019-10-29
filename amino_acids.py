2920332dd222cde2e2528832cf0c9b0d2b794e0d
B151359-2019 <email_not_found>
2019-10-29 12:04:10 +0000

    finished amino_acids.py

---
+++ amino_acids.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+def amino_acid_percentages_v1(sequence,residue):
+	try:
+		return ( sequence.upper().count(residue.upper())/len(sequence) ) * 100
+	except(AttributeError, TypeError):
+		raise AssertionError('inputs should be strings')
+
+assert round(amino_acid_percentages_v1("MSRSLLLRFLLFLLLLPPLP", "M")) == round(5)
+assert round(amino_acid_percentages_v1("MSRSLLLRFLLFLLLLPPLP", "r")) == round(10)
+assert round(amino_acid_percentages_v1("MSRSLLLRFLLFLLLLPPLP", "L")) == round(50)
+assert round(amino_acid_percentages_v1("MSRSLLLRFLLFLLLLPPLP", "Y")) == round(0)
+
+def amino_acid_percentages_v2(sequence, residue=['A','I','L','M','F','W','Y','V']):
+	try:
+		total = 0
+		total = sum((sequence.upper().count(res.upper())/len(sequence))*100 for res in residue)
+		return total
+	except(AttributeError, TypeError):
+		raise AssertionError('inputs should be strings')
+
+assert round(amino_acid_percentages_v2("MSRSLLLRFLLFLLLLPPLP", ["M"])) == 5
+assert round(amino_acid_percentages_v2("MSRSLLLRFLLFLLLLPPLP", ['F', 'S', 'L'])) == 70
+assert round(amino_acid_percentages_v2("MSRSLLLRFLLFLLLLPPLP")) == 65
+
+def base_counter(seq, threshold):
+	try:
+		count = (sum(1 for nuc in seq.upper() if nuc not in "ATCG")/len(seq))*100
+		return count >= threshold
+	except(AttributeError, TypeError):
+		raise AssertionError('Sequence should be string')
+
+assert base_counter("ATCGnnnn",50) == True
+assert base_counter("ATCGnnnn",51) == False
+assert base_counter("ATGCGTAtttTTGAGCAnnnnnnnnnnn",35) == True
+assert base_counter("ATGCGTAtttTTGAGCAnnnnnnnnnnn",50) == False
+
+def kmer_counting(dna,kmersize,minfrequency):
+	kmers = []
+	[kmers.append(dna[idx:idx+kmersize]) for idx in range(len(dna)) if len(dna[idx:idx+kmersize]) == kmersize]	
+	uniq_kmers = set(kmers)
+	[print(kmer, kmers.count(kmer)) for kmer in uniq_kmers if kmers.count(kmer) >= minfrequency]
+
+#kmer_counting("ATGCATCATG",2,2)	
+
+def kmer_user_input():
+	try:
+		dna = input("Please enter your sequence:")
+		kmersize = int(input("Please enter kmer size:"))
+		minfrequency = int(input("Please enter how many times you wish the kmer to appear:"))
+		kmer_counting(dna,kmersize,minfrequency)
+	except(AttributeError,TypeError):
+		raise AssertionError('uh oh! There was a type error somewhere. Check to make sure your dna is a string and your other variables are integers')
+kmer_user_input()
